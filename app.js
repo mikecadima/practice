66c0afb84333e756bbdac0de8367cf7445fa048f
DigitalCrafts <digitalcrafts@DCMacbookAir.local>
2020-07-23 17:04:36 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const pgp = require('pg-promise')();
+const connect = require('./config');
+const path = require("path");
+const db = pgp(connect);
+
+// const { pathToFileURL } = require('url');
+
+const port = 5005;
+
+// app.use(express.urlencoded({extended:true}))
+
+// db.any('SELECT * FROM tasks')
+// .then(data=>console.log(data))
+
+app.use(express.json())
+
+app.get("/", (req,res)=>res.sendFile(__dirname + path.join("/site/index.html")))
+
+app.get("/tasks", async (req, res)=>{
+    const data = await db.any('SELECT * FROM tasks');
+    res.json(data)
+})
+//INSERT task
+app.get("/new-task", (req,res)=>res.sendFile(__dirname + path.join("/site/newTask.html")))
+
+app.post("/new-task", async (req, res)=>{
+    if(!req.body.title) return res.send('You must supply a title.');
+    let result = await db.one(`INSERT INTO tasks (title) VALUES ('${req.body.title}') RETURNING *`)
+    res.send(result)
+})
+
+app.patch('/edit-task/:id/complete_task', async (req,res)=>{
+    let result = await db.one(`
+        UPDATE tasks 
+        SET is_completed = 'true' 
+        WHERE id='${req.params.id}' RETURNING *
+    `);
+    res.send(result);
+})
+
+app.patch('/edit-task/:id/title', )
+
+// DELETE a task
+// app.get("/tasks", (req,res)=>res.sendFile(__dirname + path.join("/site/index.html")))
+
+app.delete("/tasks/:id", (req, res) => {
+    const taskId = req.params.id;
+    let result = db.none("DELETE FROM tasks WHERE id = $1", [taskId]).then(() =>
+      res.send(`Task ${taskId} was deleted`)
+    );
+  });
+  app.patch('/edit-task/:id', async (req,res)=>{
+    let del_result = await db.one(`
+        DELETE FROM tasks
+        WHERE id = '${req.params.id}' RETURNING *
+    `);
+    res.send(result);
+});
+
+
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`)
+})
